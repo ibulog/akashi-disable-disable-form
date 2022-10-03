5a8e7c55a6d1f0b2d5c4b2664c9a4e1dc90bf003
ibulog <charo.little@gmail.com>
2022-10-04 01:41:40 +0900

    first commit

---
+++ content-script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  const id = document.getElementById("form_login_id");
+  id.disabled = false;
+
+  const password = document.getElementById("form_password");
+  password.disabled = false;
+};
